b3e4009f191e25b449ea11981248b46277ee66b0
EverthKantun <everth.ko@gmail.com>
2025-09-19 23:15:11 -0600

    feat(admin): add admin layout with Flowbite sidebar, navigation includes, and dashboard slot

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/admin');
-// Route::get('/', function () {
-//     return view('welcome');
+//  Route::get('/', function () {
+//      return view('welcome');
 // });
 
 Route::middleware([
